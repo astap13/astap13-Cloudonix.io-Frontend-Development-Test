e38de3631f971da5a213ebcfec558a460b613c03
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-07-24 18:24:44 +0200

    init library page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./sign-in/sign-in.module').then(m => m.SignInModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./sign-in/sign-in.module').then(m => m.SignInModule) }, { path: 'library', loadChildren: () => import('./library/library.module').then(m => m.LibraryModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
